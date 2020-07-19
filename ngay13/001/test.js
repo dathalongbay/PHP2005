7a294adad9c1d8d5432fcc38f30285598c02f6a4
dat <dathalongbay@gmail.com>
2020-07-19 18:30:37 +0700

    1

---
+++ ngay13/001/test.js
@@ -0,0 +1,6 @@
+
+
+console.log("--------------------- in ra 3 biến global trong 1 file khác --- ");
+console.log("Biến chu vi : " + chuvi);
+console.log("canhA : " + canhA);
+console.log("canhB : " + canhB);
