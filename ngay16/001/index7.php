64445868fe3bb97b409fce619107fc1ce015cba5
dat <dathalongbay@gmail.com>
2020-08-05 19:49:39 +0700

    111

---
+++ ngay16/001/index7.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $a = 10;
+    $b = 6;
+    echo "<br> tổng : " . ($a + $b);
+    echo "<br> hiệu : " . ($a - $b);
+    echo "<br> tích : " . ($a * $b);
+    echo "<br> thương : " . ($a / $b);
+    echo "<br> chia lấy dư : " . ($a % $b);
+    ?>
+
+</body>
+</html>
