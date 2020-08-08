db4d6546807ca7517bc4f4e430b88c771c537e04
dat <dathalongbay@gmail.com>
2020-08-08 20:29:55 +0700

    111

---
+++ ngay17/001/index6.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <?php
+    $a = [9,1,4,2,5,2,100,20,30,11,15,12];
+
+    ?>
+    <pre>
+        1 - Cho mảng trên hãy tìm ra giá trị nhỏ nhất trong mảng
+        2 - Cho mảng trên hãy tìm ra giá trị lớn nhất trong mảng
+    </pre>
+
+</body>
+</html>
