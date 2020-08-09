12949e465f29878bd33a2b1ccb83af6444646590
dat <dathalongbay@gmail.com>
2020-08-09 18:39:20 +0700

    111

---
+++ ngay18/001/index1.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <pre>
+        Xây dựng 1 hàm phục vụ yêu cầu như sau :
+        1 tài khoản ngân hàng gửi có X số tiền với lãi xuất là Y %/1 năm
+        Hãy xây dựng 1 function trả ra số tiền trong tài khoản của khách hàng
+        sau Z ngày .
+    </pre>
+
+</body>
+</html>
