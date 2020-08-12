6266fb764ab9a59927f42cd5f66a57075113835f
dat <dathalongbay@gmail.com>
2020-08-12 18:02:36 +0700

    111

---
+++ ngay19/index1.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        Xây dựng 1 ứng dụng form tính chỉ số BMI tương tự như trang
+        https://medlatec.vn/bmi-online
+        Không cần giống giao diện CSS
+    </pre>
+
+</body>
+</html>
