2eabe84bce4321b46c77e3840b094c59a5340306
dat <dathalongbay@gmail.com>
2020-08-16 19:20:06 +0700

    111

---
+++ ngay20/001/index7.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 8/16/2020
+ * Time: 7:17 PM
+ */
+
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
+
+echo "" . $_SERVER["REQUEST_URI"];
