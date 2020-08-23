365049b18ae22bcfb62047eaf1d02c2374c740a6
dat <dathalongbay@gmail.com>
2020-08-23 19:25:42 +0700

    111

---
+++ ngay22/003/index6.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        xem đáp án tại :
+        https://sites.google.com/view/tech710/php-c%C6%A1-b%E1%BA%A3n/c%C3%A1c-k%C3%BD-hi%E1%BB%87u-regex/regex1
+    </pre>
+
+</body>
+</html>
