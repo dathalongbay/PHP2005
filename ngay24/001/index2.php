016d3ef24bd827879fb05143bfc8ffc367cfcd1a
dat <dathalongbay@gmail.com>
2020-08-30 18:53:54 +0700

    123

--- ngay24/001/index2.php
+++ ngay24/001/index2.php
@@ -6,3 +6,7 @@ class TagLink {
 }
 
 // <a href="" target="" title=""></a>
+
+// tạo ra 1 class hiển thị thẻ a trong html
+// sử dụng các thuộc tính cho class này
+// và tạo 1 phương thức để hiển thị
---
+++ ngay24/001/index3.php
@@ -0,0 +1,17 @@
+<?php
+
+class Student {
+
+}
+
+// tạo ra 1 class sinh viên
+// với các thuộc tính  : tên , tuổi , quê quán , điểm học kỳ
+// thuộc tính điểm học kỳ là 1 mảng chứa 8 giá trị
+// tương ứng với 8 kỳ học
+// hãy viết 1 phương thức để tính điểm trung bình
+// sau đó in ra xếp loại cho sinh viên đó
+
+// cuối cùng tạo 2 đối tượng có tên là nam và hương
+// khởi tạo từ class sinh viên này
+// truyền các thông tin cho các thuộc tính của class
+// và gọi phương thức tính điểm TB và in ra xếp loại
