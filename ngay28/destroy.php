94a877f8cdd13b368c5cbae72c91c9047039e230
dat <dathalongbay@gmail.com>
2020-09-20 18:16:25 +0700

    111

---
+++ ngay28/destroy.php
@@ -0,0 +1,18 @@
+<?php
+define("SITE_PATH", dirname(__FILE__));
+define("SITE_URL", "http://localhost/PHP2005/ngay28/");
+
+require_once SITE_PATH."/"."connect.php";
+
+$id = isset($_GET["id"]) ? (int) $_GET["id"] : 0;
+if ($id > 0) {
+    $sql = "DELETE FROM books WHERE id = ?";
+
+    $stmt= $pdo->prepare($sql);
+
+    $stmt->execute([$id]);
+
+    header("Location: ".SITE_URL."index.php");
+    exit();
+}
+
