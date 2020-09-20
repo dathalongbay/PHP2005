d2476ea94862f954773b94c0b397e70a0ec037c2
dat <dathalongbay@gmail.com>
2020-09-20 18:17:50 +0700

    11

--- ngay28/destroy.php
+++ ngay28/destroy.php
@@ -6,6 +6,7 @@ require_once SITE_PATH."/"."connect.php";
 
 $id = isset($_GET["id"]) ? (int) $_GET["id"] : 0;
 if ($id > 0) {
+    //
     $sql = "DELETE FROM books WHERE id = ?";
 
     $stmt= $pdo->prepare($sql);
